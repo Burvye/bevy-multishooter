2dc76d4a7f93437bef0cda9c25d4fefed716f685
Burvye <thebuffonion@gmail.com>
2026-05-25 07:37:28 -0400

    Update Main Function

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,5 @@
-use avian3d::prelude::*;
 use bevy::prelude::*;
 
 fn main() -> AppExit {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(MainPlugin)
-        .run()
-}
-
-pub struct MainPlugin;
-impl Plugin for MainPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_plugins(PhysicsPlugins::default());
-    }
+    fps_proj::run()
 }
